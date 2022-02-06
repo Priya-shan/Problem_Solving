e140002069123f66ec6b14085b01ac5e0e9e5c5a
Priya-shan <Shanmugapriyashanu2002@gmail.com>
2022-02-06 09:55:33 +0530

    commit

--- Infytq/uniqueEle.java
+++ Infytq/uniqueEle.java
@@ -5,10 +5,29 @@
  */
 package Infytq;
 
+import java.io.*;
+import java.util.HashSet;
+import java.util.*;
+
 /**
  *
  * @author welcom
  */
-public class uniqueEle {
-    
+    public class uniqueEle {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n;
+            n = Integer.parseInt(br.readLine());
+        String strArr[] = br.readLine().split(",");
+        int len = strArr.length;
+        System.out.println(Arrays.toString(strArr));
+        HashSet < Integer > hs = new HashSet < > ();
+        for (int i = 0; i < len; i++) {
+            hs.add(Integer.parseInt(strArr[i]));
+            System.out.println(hs);
+        }
+        System.out.println(hs);
+        System.out.println(hs.size() - n);
+    }
 }
+
